4d84814f4efe4dcb04ba94332284a05bdf8c42cb
Pablo Bestani <pablobestanimouk@gmail.com>
2023-10-05 19:01:25 -0300

    feat(routing): created general routing config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { RootRouter } from './pages/RootRouter';
 
 function App() {
 
   return (
     <>
-
+      <RouterProvider router={RootRouter} />
+      <h1>Hello World!</h1>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ client/src/pages/RootRouter.tsx
@@ -0,0 +1,41 @@
+import { 
+    createBrowserRouter, 
+    createRoutesFromElements, 
+    Route, 
+    Link, 
+    Outlet 
+} from 'react-router-dom'
+import SignUp from './SignUp';
+import Login from './Login';
+import Profile from './Profile';
+import Applications from './Applications';
+import Spontaneous from './Spontaneous';
+
+const Root = () => {
+    return (
+      <>
+        <div>
+          <Link to="/">Profile</Link>
+          <Link to="/register">SignUp</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/applications">Applications</Link>
+          <Link to="/spontaneous">Spontaneous applications</Link>
+        </div>
+        <div>
+          <Outlet />
+        </div>
+      </>
+    )
+}
+
+export const RootRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Root />}>
+      <Route index element={<Profile />} />
+      <Route path='/register' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/applications' element={<Applications />} />
+      <Route path='/spontaneous' element={<Spontaneous />} />
+    </Route>
+  )
+);
