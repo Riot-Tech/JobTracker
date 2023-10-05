04104f24c781fe0e7806347c7cd2fe1c02c4a709
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2023-10-05 20:17:31 -0300

    jwt running

--- server/src/routes/authenticationRouter.ts
+++ server/src/routes/authenticationRouter.ts
@@ -1,12 +1,16 @@
 import { Router, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import * as bcrypt from "bcrypt";
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const authRouter: Router = Router();
 const prisma = new PrismaClient();
+const key = process.env.JWT_KEY
 
 authRouter.post("/", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body as {email: string, password: string};
   try {
     const user = await prisma.user.findFirst({
       where: {
@@ -19,10 +23,14 @@ authRouter.post("/", async (req: Request, res: Response) => {
     }
 
     if (bcrypt.compareSync(password, user.password)) { //si son iguales
-      return res.status(200).json("Login success");
+      const token = jwt.sign(user, key as string, {expiresIn: '3h'})
+      return res.status(200).json({message: "Login success", token: token });
+    }else{
+      return res.status(400).json({message: "Invalid credentials"});
     }
   } catch (error) {
-    console.log(error)
+    /* console.log(error) */
+    return res.status(400).json({message: 'Error during login, try again later'})
   }
 });
 
