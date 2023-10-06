d4c17e4942557af4be0d8bf275ae80df6de56901
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2023-10-06 10:44:43 -0300

    feat(auth/signUp handlers): password encrypted, token given to front after login

--- server/src/routes/authenticationRouter.ts
+++ server/src/routes/authenticationRouter.ts
@@ -1,37 +1,8 @@
-import { Router, Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import * as bcrypt from "bcrypt";
-import * as jwt from 'jsonwebtoken';
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { Router } from "express";
+import { authenticationHandler } from "../handlers/authenthicationHandler";
 
 const authRouter: Router = Router();
-const prisma = new PrismaClient();
-const key = process.env.JWT_KEY
 
-authRouter.post("/", async (req: Request, res: Response) => {
-  const { email, password } = req.body as {email: string, password: string};
-  try {
-    const user = await prisma.user.findFirst({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      return res.status(400).json({ error: "User doesn't exist" });
-    }
-
-    if (bcrypt.compareSync(password, user.password)) { //si son iguales
-      const token = jwt.sign(user, key as string, {expiresIn: '3h'})
-      return res.status(200).json({message: "Login success", token: token });
-    }else{
-      return res.status(400).json({message: "Invalid credentials"});
-    }
-  } catch (error) {
-    /* console.log(error) */
-    return res.status(400).json({message: 'Error during login, try again later'})
-  }
-});
+authRouter.post("/", authenticationHandler);
 
 export default authRouter;
