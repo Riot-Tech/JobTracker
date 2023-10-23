c93e068c0571a6b007fc62b44a0bd5e07220fc55
Alberto Mallar <alberto.mallar@gmail.com>
2023-10-23 08:47:56 -0300

    feat(controllers): all validations made

--- server/src/controllers/updateApplicationController.ts
+++ server/src/controllers/updateApplicationController.ts
@@ -5,37 +5,39 @@ import { Application, jobModality, jobType } from "@prisma/client"
 
 export const updateApplicationController = async (application: Application) => {
     console.log(application)
+    if (typeof application.id !== 'number') {
+        throw new Error('Wrong id type')
+    };
     if (typeof application.userId !== 'number') {
         throw new Error('Wrong userId type')
     };
-    if (typeof application.jobName !== 'string') {
+    if(application.jobName !== undefined && typeof application.jobName !== 'string') {
         throw new Error('Wrong jobName type')
     };
-    if (typeof application.company !== 'string') {
+    if (application.company !== undefined && typeof application.company !== 'string') {
         throw new Error('Wrong company type')
     };
-    if (!Object.values(jobType).includes(application.jobType)) {
+    if (application.jobType !== undefined && !Object.values(jobType).includes(application.jobType)) {
         throw new Error('Wrong job type value')
     };
-    if (!Object.values(jobModality).includes(application.jobModality)) {
+    if (application.jobModality !== undefined && !Object.values(jobModality).includes(application.jobModality)) {
         throw new Error('Wrong modality value')
     };
-    if (typeof application.location !== 'string') {
+    if (application.location !== undefined && typeof application.location !== 'string') {
         throw new Error('Wrong location type')
     };
-    if (typeof application.expectedIncome !== 'number') {
+    if (application.expectedIncome !== undefined && typeof application.expectedIncome !== 'number') {
         throw new Error('Wrong expectedIncome type')
     };
-    if (typeof application.currency !== 'string') {
+    if (application.currency !== undefined && typeof application.currency !== 'string') {
         throw new Error('Wrong currency type')
     };
-    if (typeof application.feedback !== 'string') {
+    if (application.feedback !== undefined && typeof application.feedback !== 'string') {
         throw new Error('Wrong feedback type')
     };
-    if (typeof application.comments !== 'string') {
-        throw new Error('Wrong company type')
+    if (application.comments !== undefined && typeof application.comments !== 'string') {
+        throw new Error('Wrong comments type')
     };
     const updateApplication = await updateApplicationHelper(application)
     return updateApplication
-} // para controlar las validaciones de los update tengo que verificar que el tipo de dato
-// ingresado sea correcto pero yo no sé qué me va a entrar. 
+} 
