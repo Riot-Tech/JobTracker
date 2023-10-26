ef002117337e1c02f60c613d6e66c057d5854889
albertog1 <alberto.gentile1@gmail.com>
2023-10-26 13:01:14 -0300

    fix(Application):added user ID for searching all applications

--- server/src/controllers/ApplicationControllers/getApplicationsController.ts
+++ server/src/controllers/ApplicationControllers/getApplicationsController.ts
@@ -2,8 +2,9 @@ import { getApplicationsHelper } from "../../helpers"
 
 
 
-export const getApplicationsController = async () => {
-    const allApplications = await getApplicationsHelper();
+export const getApplicationsController = async (id: number) => {
+    if (typeof id !== 'number') throw new Error('Wrong ID type');
+    const allApplications = await getApplicationsHelper(id);
     if (allApplications) return allApplications;
     throw Error ('Applications not found at getApplicationController');
 }
