1aac1a09a1aafe4cd37f7451b0b00d680b409a67
albertog1 <alberto.gentile1@gmail.com>
2023-10-26 17:14:28 -0300

    feat(Link):Information validations added

--- server/src/controllers/LinkControllers/updateLinkController.ts
+++ server/src/controllers/LinkControllers/updateLinkController.ts
@@ -3,6 +3,11 @@ import { updateLinkHelper } from "../../helpers"
 
 
 export const updateLinkController = async (link: Link) => {
+    if (typeof link.userId !== 'number') throw new Error('Wrong userId type');
+    if (typeof link.appId !== 'number') throw new Error('Wrong appId type');
+    if (typeof link.spontId !== 'number') throw new Error('Wrong sponId type');
+    if (typeof link.name !== 'string') throw new Error('Wrong name type');
+    if (typeof link.url !== 'string') throw new Error('Wrong url type');
     const updatedLink = await updateLinkHelper(link);
     if (updatedLink) return updatedLink;
     throw Error('Updated link not found at updateLinkController');
