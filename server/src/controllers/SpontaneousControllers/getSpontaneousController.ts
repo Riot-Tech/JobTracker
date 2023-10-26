af60fe5ea8c2fc8bf03dc55ab71952f29aa3b993
albertog1 <alberto.gentile1@gmail.com>
2023-10-26 13:01:47 -0300

    fix(Spontaneous):added user ID for searching all spontaneous applications

--- server/src/controllers/SpontaneousControllers/getSpontaneousController.ts
+++ server/src/controllers/SpontaneousControllers/getSpontaneousController.ts
@@ -1,8 +1,9 @@
 import { getSpontaneousHelper } from "../../helpers"
 
 
-export const getSpontaneousController = async () => {
-    const allSpontaneous = await getSpontaneousHelper();
+export const getSpontaneousController = async (id: number) => {
+    if (typeof id !== 'number') throw new Error('Wrong ID type');
+    const allSpontaneous = await getSpontaneousHelper(id);
     if (allSpontaneous) return allSpontaneous;
     throw Error('Spontaneous applications not found at getSpontaneousController');
 }
