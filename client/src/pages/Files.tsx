8657f41a0feed41714d51e9ef5862c9691dbd8e1
Pablo Bestani <pablobestanimouk@gmail.com>
2023-11-15 10:43:43 -0300

    feat(cloudStorage): basic frontend component for file upload

--- client/src/pages/Files.tsx
+++ client/src/pages/Files.tsx
@@ -1,14 +1,48 @@
-import React, { useState } from 'react'
+import { useState, ChangeEvent } from 'react'
 import SideBar from '../components/SideBar'
 import NavBar from '../components/NavBar'
 import { NewFileIcon } from '../utils/svg'
+import axios from 'axios';
+import { URL } from "../utils/url";
 
 function Files() {
-    const [ modalOpen, setModalOpen]= useState<boolean>(false)
-    
-    const handleClick = () => {
-      setModalOpen(!modalOpen)
+  const [ modalOpen, setModalOpen]= useState<boolean>(false)
+  const handleClick = () => {
+    setModalOpen(!modalOpen)
+  }
+  
+  //////////////////////////////
+  // LO QUE IRIA EN EL MODAL::
+  const [file, setFile] = useState<File | null>(null);
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    let chosenFile = null;
+    if (e.target.files) {
+      chosenFile = e.target.files[0];
+      setFile(chosenFile);
+    } else console.log("No file found");
+  }
+
+  const handleUpload = async () => {
+    //Este es un ejemplo de fileData
+    const fileData = {
+      userId: 1, //reemplazar por el id del activeUser
+      name: "myFirstFile", //reemplazar por el name que el usuario quiera poner en el modal
+      url: '', //queda como string vacio, el backend la va a llenar despues
+      isCv: false //el usuario decide ponerlo en true si quiere, sino va en false
     }
+    
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('data', JSON.stringify(fileData));
+      
+      const res = await axios.post(`${URL}/file`, formData);
+      if (res) return console.log(res);
+    }
+  };
+  // Agregar tambien el input y el button que estan mas abajo
+  //////////////////////////////
+
   return (
     <div className='flex'>
         <SideBar/>
@@ -34,6 +68,9 @@ function Files() {
               </div>
     
             </div>
+            {/* estos tambien irian en el modal: */}
+            <input type="file" onChange={handleFileChange}/>
+            <button onClick={handleUpload}>Upload</button>
         </div>
     </div>
   )
