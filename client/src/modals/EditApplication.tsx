f919edfcf14e0b0ecef1d5cc5ec13e9976fc5edf
Lorenzo Martin del Hoyo <delhoyo.lorenzo@gmail.com>
2023-12-07 18:53:34 -0300

    style(editApp): added responsive

--- client/src/modals/EditApplication.tsx
+++ client/src/modals/EditApplication.tsx
@@ -7,7 +7,13 @@ import { useSelector } from "react-redux";
 import { AppStore, Application } from "../models/interfaces";
 import { getApplications } from "../redux/slices/applications.slice";
 import { AiOutlineClose } from "react-icons/ai";
-import { CommentIcon, FeedbackIcon, JobModalityIcon, JobTypeIcon, LinkIcon, LocationIcon, MoneyIcon, TickIcon, WalletIcon } from "../utils/svg";
+import { TickIcon } from "../utils/svg";
+import { FaLocationDot, FaRegCommentDots } from "react-icons/fa6";
+import { RiArrowGoBackFill, RiMoneyDollarCircleLine } from "react-icons/ri";
+import { MdOutlinePendingActions, MdOutlineWatchLater } from "react-icons/md";
+import { IoAlertCircle, IoWalletOutline } from "react-icons/io5";
+import { CiLink } from "react-icons/ci";
+import { FaHome } from "react-icons/fa";
 
 type CloseFunction = () => void;
 
@@ -96,61 +102,68 @@ export default function EditApplication({ close, props }: { close: CloseFunction
         }
     }
 
+    const hasErrors = Object.values(errors).some((error) => !!error);
+
     return (
-        <form className="fixed inset-0 z-20 flex flex-col items-center justify-center backdrop-brightness-90 backdrop-blur-sm">
+        <form className="fixed inset-0 flex flex-col items-center justify-center z-20 backdrop-brightness-90 backdrop-blur-sm">
             <AiOutlineClose onClick={close} className='text-4xl text-white bg-black rounded-2xl p-1 mb-4 hover: cursor-pointer hover:bg-gray-600 dark:bg-white dark:text-black dark:hover:bg-gray-400'/>
-            <div className="flex h-[80vh] w-[80vw] rounded-xl text-black bg-custom-modalLight dark:text-white dark:bg-custom-modalDark">
-                <div className="flex flex-col justify-between w-[50vw] p-5">
-                    <div>
-                        <div className="flex items-center divide-black pb-5">
+            <div className="flex h-[80%] w-[80%] rounded-xl text-black bg-custom-modalLight dark:text-white dark:bg-custom-modalDark overflow-y-auto">
+                
+                <div className="flex flex-col p-5 w-1/2">
+                    
+                    <div className=" bg-red divide-black w-full">
+                        <div className="flex items-center pb-5">
                             <input
-                                className={`mr-1 p-1 bg-transparent border-b-2 border-black ${errors.company.length && 'bg-black border-2 border-red-700 rounded-md'}`}
+                                className={`w-[60%] mr-1 p-1 bg-transparent border-b-2 border-black ${errors.company.length && 'bg-black border-2 border-red-700 rounded-md'}`}
                                 name="company"
                                 type="text"
-                                placeholder={company}
+                                placeholder="Company Name"
                                 value={form.company}
                                 onChange={handleChange}
                             />
+                            {/* {errors.company && <span className="text-red-800" > {errors.company}</span>} */}
                         </div>
                         <div className="flex items-center pb-5">
                             <input
-                                className={`mr-1 p-1 bg-transparent border-b-2 border-black ${errors.jobName.length && 'bg-black border-2 border-red-700 rounded-md'}`}
+                                className={`w-[60%] mr-1 p-1 bg-transparent border-b-2 border-black ${errors.jobName.length && 'bg-black border-2 border-red-700 rounded-md'}`}
                                 name="jobName"
                                 type="text"
-                                placeholder={jobName}
+                                placeholder="Job Name"
                                 value={form.jobName}
                                 onChange={handleChange}
                             />
                         </div>
                     </div>
-                    <div className="flex flex-col gap-4">
-                        <div className="flex items-center">
-                            <JobTypeIcon />
+                    
+                    <div className="flex flex-col gap-[1em]">
+                        <div className={`flex items-center  `}>
+                            <MdOutlineWatchLater className='' />
                             <select
                                 name="jobType"
-                                className="text-gray-600 p-2 ml-4 rounded-xl"
+                                className="w-1/2 text-gray-600 ml-2 rounded-xl p-3  "
                                 value={form.jobType}
                                 onChange={handleChange}
                             >
                                 <optgroup label="Job Type">
-                                    <option> {jobType} </option>
+                                    <option> Job Type </option>
                                     <option> FULLTIME </option>
                                     <option> PART_TIME </option>
                                     <option> FREELANCE </option>
                                     <option> UNSPECIFIED </option>
                                 </optgroup>
+                                { errors.jobType && <IoAlertCircle className='items-center text-3xl text-red-600 ml-1' />}
                             </select>
                         </div>
-                        <div className="flex items-center">
-                            <JobModalityIcon />
+                        <div className={`flex items-center `}>
+                            <FaHome />
                             <select
                                 name="jobModality"
-                                className="text-gray-600 p-2 ml-4 rounded-xl" 
+                                className="w-1/2 text-gray-600 ml-2 rounded-xl p-3"
                                 value={form.jobModality}
                                 onChange={handleChange}
                             >
                                 <optgroup label="Job Modality">
-                                    <option> {jobModality} </option>
+                                    <option> Job Modality </option>
                                     <option> REMOTE </option>
                                     <option> ONSITE </option>
                                     <option> HYBRID </option>
@@ -158,107 +171,114 @@ export default function EditApplication({ close, props }: { close: CloseFunction
                                 </optgroup>
                             </select>
                         </div>
-                        <div className="flex items-center">
-                            <LocationIcon />
+                        <div className={`flex items-center `}>
+                            <FaLocationDot />
                             <input
                                 name="location"
-                                className="p-2 border rounded-xl ml-4 text-black"
+                                className="w-1/2 ml-2 rounded-xl p-3 text-black"
                                 type="text"
-                                placeholder={location}
+                                placeholder="Location"
                                 value={form.location}
                                 onChange={handleChange}
                             />
-                            {errors.location && <span className="text-red" > {errors.location}</span>}
+                            {errors.location && <IoAlertCircle className='items-center text-3xl text-red-600 ml-1' />}
                         </div>
-                        <div className="flex items-center">
-                            <LinkIcon />
+                        <div className={`flex items-center `}>
+                            <CiLink />
                             <input
                                 name="link"
-                                className="p-2 border rounded-xl ml-4 text-black"
+                                className="w-1/2 ml-2 rounded-xl p-3 border text-black"
                                 type="text"
-                                placeholder={link}
+                                placeholder="Link"
                                 value={form.link}
                                 onChange={handleChange}
                             />
                         </div>
-                        <div className="flex items-center">
-                            <MoneyIcon />
+                        <div className={`flex items-center `}>
+                            <RiMoneyDollarCircleLine />
                             <input
                                 name="currency"
-                                className="p-2 border rounded-xl ml-4 text-black"
+                                className="w-1/2 ml-2 rounded-xl p-3 text-black"
                                 type="text"
-                                placeholder={currency}
+                                placeholder="Currency"
                                 value={form.currency}
                                 onChange={handleChange}
                             />
-                            {errors.currency && <span className="text-red" > {errors.currency}</span>}
+                            {errors.currency && <IoAlertCircle className='items-center text-3xl text-red-600 ml-1' />}
                         </div>
-                        <div className="flex items-center">
-                            <WalletIcon />
+                        <div className={`flex items-center            `}>
+                            <IoWalletOutline />
                             <input
                                 name="expectedIncome"
-                                className="flex flex-col p-2 border rounded-xl ml-4 text-black"
+                                className="w-1/2 ml-2 rounded-xl p-3 text-black"
                                 type="number"
-                                placeholder={String(expectedIncome)}
+                                placeholder="Expected Income"
                                 value={form.expectedIncome}
                                 onChange={handleChange}
                             />
-                            {errors.expectedIncome && <span className="text-red" > {errors.expectedIncome}</span>}
+                            {errors.expectedIncome && <IoAlertCircle className='items-center text-3xl text-red-600 ml-1' />}
                         </div>
-                        <div className="flex items-center">
-                            <JobModalityIcon/>
+                        {/* ${style.inputContainer} */}
+                        <div className={`flex items-center `}>
+                            <MdOutlinePendingActions />
                             <select
                                 name="status"
-                                className="text-gray-600 p-2 ml-4 rounded-xl"
+                                className="w-1/2 text-gray-600 p-3 ml-2 rounded-xl"
                                 value={form.status}
                                 onChange={handleChange}
                             >
                                 <optgroup label="Status">
-                                    <option> {status} </option>
+                                    <option> Status </option>
                                     <option> PENDING </option>
                                     <option> SUBMITTED </option>
                                     <option> INTERVIEW_SCHEDULED </option>
                                     <option> REJECTED </option>
                                 </optgroup>
                             </select>
+                            {errors.status && <IoAlertCircle className='items-center text-3xl text-red-600 ml-1' />}
                         </div>
                     </div>
                 </div>
-                <div className="w-[50%] flex flex-col p-5">
-                    <div className="flex justify-end ">
+                
+                <div className="w-[50%] flex flex-col p-5 pl-0 h-full">
+                    <div className="flex justify-end">
                         <button
                             type="submit"
+                            disabled={hasErrors || Object.values(errors).some((error) => error !== '')}
                             onClick={handleSubmit}
                             className={`flex items-center ${confirmed ? 'bg-green-400 ring ring-green-400' : 'bg-red-500'}`}
-                        >
-                          
-                            <TickIcon/>
+                            >
+                            {<TickIcon/>}
                             <h2 className='ml-1 text-white'>Confirm</h2>
                         </button>
                     </div>
-                    <div className="flex items-center mt-5">
-                        <FeedbackIcon />
-                        <label className="ml-4"> Feedback </label>
-                    </div>
-                    <textarea
-                        name="feedback"
-                        value={form.feedback}
-                        className=" h-[180px] mt-3 text-black p-2"
-                        onChange={handleChange}
-                        placeholder={feedback}
-                    />
-                    <div className="flex flex-col mt-5">
+
+                    <div className="flex flex-col mt-5 p-1 h-full"> 
                         <div className="flex items-center">
-                            <CommentIcon />
+                            <RiArrowGoBackFill className='text-2xl' />
+                            <label className="ml-4"> Feedback </label>
+                        </div>
+                        <textarea
+                            name="feedback"
+                            value={form.feedback}
+                            className="h-full text-black p-2 mt-3"
+                            onChange={handleChange}
+                            placeholder="Any feedback from the company (e.g., interview experience, comments, or follow-up notes)"
+                        />
+                    </div>
+
+                    <div className="flex flex-col mt-5 p-1 h-full">
+                        <div className="flex items-center">
+                            <FaRegCommentDots className='text-2xl' />
                             <label className="ml-4"> Comments </label>
                         </div>
 
                         <textarea
                             name="comments"
                             value={form.comments}
-                            className=" h-[180px] mt-3 text-black p-2"
+                            className="h-full text-black p-2 mt-3"
                             onChange={handleChange}
-                            placeholder={comments}
+                            placeholder="Feel free to write any comments you'd like here."
                         />
                     </div>
                 </div>
